582e343b7cea0b46ef009c234fa5cf4bbb7de4bf
SergoDobro <optimgrey@yandex.ru>
2022-07-31 15:09:55 +0300

    It's done

--- RedactorBeta/LoginsAndPasswords.xaml.cs
+++ RedactorBeta/LoginsAndPasswords.xaml.cs
@@ -72,6 +72,30 @@ namespace RedactorBeta
         }
         public void Save(object obj, RoutedEventArgs ev)
         {
+            for (int i = 0; i < loginsAndPasswords.Children.Count-1; i++)
+            {
+                bool isOk = true;
+                for (int j = 0; j < loginsAndPasswords.Children.Count - 1; j++)
+                {
+                    if (i!=j)
+                    {
+                        if ((loginsAndPasswords.Children[i] as ComboBoxInput).login.Text == (loginsAndPasswords.Children[j] as ComboBoxInput).login.Text)
+                        {
+                            (loginsAndPasswords.Children[j] as ComboBoxInput).login.BorderBrush = Brushes.Red;
+                            isOk = false;
+                        }
+
+                    }
+                }
+                if (isOk)
+                {
+                    (loginsAndPasswords.Children[i] as ComboBoxInput).login.BorderBrush = new SolidColorBrush(Color.FromRgb(171, 173, 179));
+                }
+                else
+                {
+                    (loginsAndPasswords.Children[i] as ComboBoxInput).login.BorderBrush = Brushes.Red;
+                }
+            }
             OnSave();
         }
     }
